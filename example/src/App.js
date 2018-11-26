1a7d3d1fb401a6f3d755df98d641ab7ae594e53b
Jorge Adolfo <7jagjag@gmail.com>
2018-11-27 00:11:20 +0100

    update example

--- example/src/App.js
+++ example/src/App.js
@@ -1,33 +1,46 @@
-import React, { useState } from 'react'
-
-import { createOnKeyDownBinding } from 'key-bidings'
+import React, { useState } from "react";
+import { createOnKeyDownBinding } from "key-bidings";
 
 export default () => {
-  const { inputProps, addToChat, messages } = useChat()
+  const { inputProps, addToChat, messages } = useChat();
   const onKeyDown = createOnKeyDownBinding({
-    '*-Enter': (e) => {
-      e.preventDefault()
-      console.log('saving...')
-      addToChat()
+    "*-Enter": e => {
+      e.preventDefault();
+      console.log("saving...");
+      addToChat();
     }
-  })
+  });
 
-  return <div className="container" style={{ marginTop: '40px' }}>
-    <textarea className="form-control" onKeyDown={onKeyDown} {...inputProps} />
-    <hr />
-    {messages.map(message => <p className="text-primary" style={{padding: '10px', border: '1px solid #ccc'}}>{message}</p>)}
-  </div>
-}
+  return (
+    <div className="container">
+      <h4>chatroom 👨🏻‍💻</h4>
+      <textarea
+        className="form-control"
+        onKeyDown={onKeyDown}
+        {...inputProps}
+      />
+      {!messages.length && (
+        <p class="empty">press cmd-enter or ctrl-enter to add a message</p>
+      )}
+      {messages.map(message => (
+        <pre className="message">{message}</pre>
+      ))}
+    </div>
+  );
+};
 
 // --------------------------------------------
 function useChat() {
-  const [message, setMessage] = useState('')
-  const [history, setHistory] = useState([])
+  const [message, setMessage] = useState("");
+  const [history, setHistory] = useState([]);
 
-  const inputProps = { value: message, onChange: (e) => setMessage(e.target.value) }
+  const inputProps = {
+    value: message,
+    onChange: e => setMessage(e.target.value)
+  };
   const addToChat = () => {
-    setHistory([message, ...history])
-    setMessage('')
-  }
-  return { inputProps, addToChat, messages: history }
+    setHistory([message, ...history]);
+    setMessage("");
+  };
+  return { inputProps, addToChat, messages: history };
 }
